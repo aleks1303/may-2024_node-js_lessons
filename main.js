8cc19b451f0139da8b39bc90886b7a391b93f321
aleks1303 <aleksbulda13@gmail.com>
2024-12-09 21:06:50 +0200

    nodejs-lesson1_hw-1-module_http

--- main.js
+++ main.js
@@ -8,6 +8,28 @@ const bar = async () => {
 // Create a local server to receive data from
     const server = http.createServer((req, res) => {
         res.writeHead(200, { 'Content-Type': 'application/json' });
+
+        const url = req.url;
+        if (url === '/about'){
+            // в середині можна використовувати різні методи
+            switch (req.method) {
+                case 'GET': res.end(JSON.stringify({
+                    data: 'About PRO Page!'
+                }));
+
+                case 'POST': res.end(JSON.stringify({
+                    data: 'AboutPro Page!'
+                }));
+            }
+
+        }
+        if (url === '/contact'){
+            res.end(JSON.stringify({
+                data: 'Contact Page!'
+            }));
+            return
+        }
+
         res.end(JSON.stringify({
             data: 'Hello World!',
         }));
