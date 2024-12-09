c7ac12a11b4eb592789d048acc6da1cd279a1296
aleks1303 <aleksbulda13@gmail.com>
2024-12-09 14:00:09 +0200

    nodejs-lesson1_intro_event-loop

--- main.js
+++ main.js
@@ -1,34 +1,3 @@
-// require - це import file helpers.js
-// require - записуємо в const і витягуємо функцію foo
-// const {foo} = require('./helpers/helper')
-//
-// console.log('_____________main.js___________')
-//
-// console.log(__dirname)
-// console.log(__filename)
-//
-// // вказує в якій директорії знаходиться на момент запуску
-// console.log(process.cwd())
-//
-// // тут викликаємо імпортовану функцію foo
-// foo()
-
-
-// це модуль, який задає питання, потім на відповідь він відповідає
-const readline = require('node:readline')
-
-const bar = async () => {
-const rlInstance = readline.createInterface({
-    input:process.stdin,
-    output:process.stdin,
-
-})
-    rlInstance.question('What is your name?', (name) => {
-        console.log(`Hello ${name}`);
-        rlInstance.close()
-    })
-}
-void bar()
 
 
 
